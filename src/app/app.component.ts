883d86ce1513cf9c4b06e4a58eae24c336e7f07f
Asree(FrontEnd) <asree.h@rmutsvmail.com>
2022-07-23 21:04:40 +0700

    line-liff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { LineLiffService } from './service/line-liff/line-liff.service';
+
+declare var liff: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FrondEnd';
-}
+  messages: string;
+  userProfile: any;
+  constructor(private lineLiffService: LineLiffService) {
+    this.messages = "";
+    this.initLineLiff();
+  }
 
+  async ngOnInit() {
+    await this.initLineLiff();
+  }
+
+  async initLineLiff() {
+    try {
+      const data: any = await this.lineLiffService.initLineLiff();
+      this.userProfile = await liff.getProfile();
+      alert(`Hi ${this.userProfile.displayName}!`);
+    } catch (err) {
+      // alert(err)
+    }
+  }
+
+  async sendMessages() {
+    try {
+      const successMsgs = await liff.sendMessages([
+        {
+          type: 'text',
+          text: this.messages
+        }
+      ])
+      liff.closeWindow()
+
+
+    } catch (e) {
+      alert(e)
+    }
+
+  }
+}
--- src/app/service/line-liff/line-liff.service.ts
+++ src/app/service/line-liff/line-liff.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+declare var liff: any;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,5 +9,23 @@ export class LineLiffService {
 
   constructor() { }
 
-  
+  initLineLiff() {
+    return new Promise((resolve, reject) => {
+      liff.init((data: any) => {
+        resolve(liff.getProfile())
+      }, (err: any)=>{
+        reject(err)
+      })
+    })
+  }
+
+  getLineProfile() {
+    return new Promise((resolve, reject) => {
+      liff.getProfile((data: unknown) => {
+        resolve(data)
+      }, (err: any)=>{
+        reject(err)
+      })
+    })
+  }
 }
