54804f94312affd176d9144761cdccddb724e65f
muhammadhisam pala <muhammadhisam@miwt.biz>
2022-08-15 17:04:41 +0700

    add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,22 @@ import { ReserveComponent } from './components/reserve/reserve/reserve.component
 import { HistoryReserveComponent } from './components/history-reserve/history-reserve.component';
 import { CalendarReserveComponent } from "./components/calendar-reserve/calendar-reserve.component";
 import { MenuComponent } from './components/menu/menu.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
 
 const routes: Routes = [
-  {path:'',pathMatch:'full',redirectTo: 'register'},
-  {path:'register',component: RegisterComponent},
-  {path:'menu',component: MenuComponent},
-  {path:'calendar-reserve', component: CalendarReserveComponent},
-  {path:'reserve',component: ReserveComponent},
-  {path:'history-reserve',component: HistoryReserveComponent},
-  {path:'calendar-reserve',component: CalendarReserveComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'register' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'calendar-reserve', component: CalendarReserveComponent },
+  { path: 'reserve', component: ReserveComponent },
+  { path: 'history-reserve', component: HistoryReserveComponent },
+  { path: 'calendar-reserve', component: CalendarReserveComponent },
+  { path: '**', redirectTo: 'page404' },
+  { path: 'page404', component: ErrorpageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
